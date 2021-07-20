f9b7cb11d885c1c8721a971270bfe01538807baa
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-07-21 02:56:18 +1200

    First working version of RegisterScan

--- RegisterScan/Program.cs
+++ RegisterScan/Program.cs
@@ -1,12 +1,98 @@
-﻿using System;
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) July 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System;
+using System.Device.Gpio;
+using System.Device.Spi;
+using System.Diagnostics;
+using System.Threading;
 
-namespace RegisterScan
+namespace devMobile.IoT.SX127x.RegisterScan
 {
+	public sealed class SX127XDevice
+	{
+		private readonly int SpiBusId;
+		private readonly int ChipSelectLogicalPinNumber;
+		private readonly SpiDevice SX127XTransceiver = null;
+		private readonly GpioController gpioController = null;
+
+		public SX127XDevice(int spiBusId = 0, int chipSelectLine = 0, int chipSelectLogicalPinNumber = 0)
+		{
+			this.SpiBusId = spiBusId;
+			this.ChipSelectLogicalPinNumber = chipSelectLogicalPinNumber;
+
+			var settings = new SpiConnectionSettings(spiBusId, chipSelectLine)
+			{
+				ClockFrequency = 500000,
+				Mode = SpiMode.Mode0,
+			};
+
+			this.SX127XTransceiver = SpiDevice.Create(settings);
+
+			Thread.Sleep(500);
+
+			if (ChipSelectLogicalPinNumber != 0)
+			{
+				gpioController = new GpioController(PinNumberingScheme.Logical);
+				gpioController.OpenPin(ChipSelectLogicalPinNumber, PinMode.Output);
+				gpioController.Write(ChipSelectLogicalPinNumber, PinValue.High);
+			}
+		}
+
+		public Byte RegisterReadByte(byte registerAddress)
+		{
+			Span<byte> writeBuffer = stackalloc byte[] { registerAddress, 0 };
+			Span<byte> readBuffer = stackalloc byte[writeBuffer.Length];
+			Debug.Assert(SX127XTransceiver != null);
+
+			if (this.ChipSelectLogicalPinNumber != 0)
+			{
+				gpioController.Write(ChipSelectLogicalPinNumber, PinValue.Low);
+			}
+
+			this.SX127XTransceiver.TransferFullDuplex(writeBuffer, readBuffer);
+
+			if (this.ChipSelectLogicalPinNumber != 0)
+			{
+				gpioController.Write(ChipSelectLogicalPinNumber, PinValue.High);
+			}
+
+			return readBuffer[writeBuffer.Length - 1];
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//const int ChipSelectLogicalPinNumber = 25;
+			//SX127XDevice sX127XDevice = new SX127XDevice(chipSelectLine:0, chipSelectLogicalPinNumber= ChipSelectLogicalPinNumber);
+			SX127XDevice sX127XDevice = new SX127XDevice(chipSelectLine: 0);
+
+			while (true)
+			{
+				for (byte registerIndex = 0; registerIndex <= 0x42; registerIndex++)
+				{
+					byte registerValue = sX127XDevice.RegisterReadByte(registerIndex);
+
+					Debug.WriteLine("Register 0x{0:x2} - Value 0x{1:x2} - Bits {2}", registerIndex, registerValue, Convert.ToString(registerValue, 2).PadLeft(8, '0'));
+				}
+
+				Thread.Sleep(10000);
+			}
 		}
 	}
 }
