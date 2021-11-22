e607818491fbe76478d30503647d6f773b828810
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-11-22 19:43:10 +1300

    Fixed issue disovered by @GPT-John in the way RegOpMode was managed after transmission completed

--- SX127XLoRaDeviceClient/Program.cs
+++ SX127XLoRaDeviceClient/Program.cs
@@ -26,53 +26,55 @@ namespace devMobile.IoT.SX127XLoRaDeviceClient
 	{
 		class Program
 		{
+			private static SX127XDevice sX127XDevice;
+
 			static void Main(string[] args)
 			{
 				const double Frequency = 915000000.0;
 				int messageCount = 1;
 
 #if ADAFRUIT_RADIO_BONNET
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 22, resetLogicalPinNumber: 25);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 22, resetLogicalPinNumber: 25);
 #endif
 
 #if DRAGINO_CS0 // Y
-				SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 #if DRAGINO_CS1 // Y
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 #if ELECROW
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
 #endif
 
 #if ELECTRONIC_TRICKS
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
+				SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
 #endif
 
 #if M2M_CS0
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 #if M2M_CS1
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 				// Uptronics RPI Zero has no reset pin has switch selectable CS0 or CS1
 #if UPUTRONICS_RPIZERO_CS0
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
 #endif
 #if UPUTRONICS_RPIZERO_CS1
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
 #endif
 
 				// Uptronics RPI Plus can have two devices one on CS0 the other on CS1
 #if UPUTRONICS_RPIPLUS_CS0 && !UPUTRONICS_RPIPLUS_CS1
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
 #endif
 #if UPUTRONICS_RPIPLUS_CS1 && UPUTRONICS_RPIPLUS_CS0
-			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
+				sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
 #endif
 
 				sX127XDevice.Initialise(SX127XDevice.RegOpModeMode.ReceiveContinuous, 
@@ -121,6 +123,8 @@ namespace devMobile.IoT.SX127XLoRaDeviceClient
 
 			private static void SX127XDevice_OnTransmit(object sender, SX127XDevice.OnDataTransmitedEventArgs e)
 			{
+				sX127XDevice.SetMode(SX127XDevice.RegOpModeMode.ReceiveContinuous);
+
 				Console.WriteLine("{0:HH:mm:ss}-TX Done", DateTime.Now);
 			}
 		}
