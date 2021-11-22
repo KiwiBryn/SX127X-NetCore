ea1eca54722eddc2dbfc4c0dfe0379987e4b8bf6
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-11-22 17:56:12 +1300

    Got rid of all the Arduino-LoRa libeary sample integration cut back to minimum

--- SX127XLoRaDeviceClient/Program.cs
+++ SX127XLoRaDeviceClient/Program.cs
@@ -22,201 +22,107 @@ using devMobile.IoT.SX127xLoRaDevice;
 
 namespace devMobile.IoT.SX127XLoRaDeviceClient
 {
-	class Program
+	namespace devMobile.IoT.SX127XLoRaDeviceClient
 	{
+		class Program
+		{
+			static void Main(string[] args)
+			{
+				const double Frequency = 915000000.0;
+				int messageCount = 1;
+
 #if ADAFRUIT_RADIO_BONNET
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 22, resetLogicalPinNumber: 25);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 22, resetLogicalPinNumber: 25);
 #endif
 
-#if DRAGINO_CS0
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+#if DRAGINO_CS0 // Y
+				SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
-#if DRAGINO_CS1
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+#if DRAGINO_CS1 // Y
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 #if ELECROW
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
 #endif
 
+#if ELECTRONIC_TRICKS
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 25, resetLogicalPinNumber: 22);
+#endif
 
 #if M2M_CS0
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
 #if M2M_CS1
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, interuptLogicalPinNumber: 4, chipSelectLogicalPinNumber: 25, resetLogicalPinNumber: 17);
 #endif
 
-		// Uptronics RPI Zero has no reset pin has switch selectable CS0 or CS1
+				// Uptronics RPI Zero has no reset pin has switch selectable CS0 or CS1
 #if UPUTRONICS_RPIZERO_CS0
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
 #endif
 #if UPUTRONICS_RPIZERO_CS1
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
 #endif
 
-		// Uptronics RPI Plus can have two devices one on CS0 the other on CS1
+				// Uptronics RPI Plus can have two devices one on CS0 the other on CS1
 #if UPUTRONICS_RPIPLUS_CS0 && !UPUTRONICS_RPIPLUS_CS1
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS0, 25);
 #endif
 #if UPUTRONICS_RPIPLUS_CS1 && UPUTRONICS_RPIPLUS_CS0
-		private readonly static SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
+			SX127XDevice sX127XDevice = new SX127XDevice(SX127XDevice.ChipSelectLine.CS1, 16);
 #endif
 
-		static void Main(string[] args)
-		{
-			int messageCount = 1;
-
-			sX127XDevice.Initialise(
-					SX127XDevice.RegOpModeMode.ReceiveContinuous,
-					915000000.0,
-					powerAmplifier: SX127XDevice.PowerAmplifier.PABoost,
-					// outputPower: 5, outputPower: 20, outputPower:23,
-					//powerAmplifier: SX127XDevice.PowerAmplifier.Rfo,	
-					//outputPower:-1, outputPower: 14,
-#if LORA_SENDER // From the Arduino point of view
+				sX127XDevice.Initialise(SX127XDevice.RegOpModeMode.ReceiveContinuous, 
+					Frequency,
+					powerAmplifier: SX127XDevice.PowerAmplifier.PABoost, 
+					rxPayloadCrcOn: true, 
 					rxDoneignoreIfCrcMissing: false
-#endif
-#if LORA_RECEIVER // From the Arduino point of view, don't actually need this as already inverted
-					invertIQTX: true
-#endif
-
-#if LORA_SET_SYNCWORD
-					syncWord: 0xF3,
-					invertIQTX: true,
-					rxDoneignoreIfCrcMissing: false
-#endif
-#if LORA_SET_SPREAD
-					spreadingFactor: SX127XDevice.RegModemConfig2SpreadingFactor._256ChipsPerSymbol,
-					invertIQTX: true,
-					rxDoneignoreIfCrcMissing: false
-#endif
-#if LORA_SIMPLE_NODE // From the Arduino point of view
-					invertIQTX: false,
-					rxDoneignoreIfCrcMissing: false
-#endif
-#if LORA_SIMPLE_GATEWAY // From the Arduino point of view
-					invertIQRX: true,
-					rxDoneignoreIfCrcMissing: false
-#endif
-#if LORA_DUPLEX
-					rxPayloadCrcOn: true
-#endif
 					);
 
 #if DEBUG
-			sX127XDevice.RegisterDump();
+				sX127XDevice.RegisterDump();
 #endif
 
-#if !LORA_RECEIVER
-			sX127XDevice.OnReceive += SX127XDevice_OnReceive;
-			sX127XDevice.Receive();
-#endif
-#if !LORA_SENDER
-			sX127XDevice.OnTransmit += SX127XDevice_OnTransmit;
-#endif
-
-#if LORA_SENDER
-			Thread.Sleep(-1);
-#else
-			Thread.Sleep(5000);
-#endif
-
-			while (true)
-			{
-				string messageText = "Hello LoRa from .NET Core! " + messageCount.ToString();
-
-#if LORA_DUPLEX
-				byte[] messageBytes = new byte[messageText.Length+4];
-
-				messageBytes[0] = 0xaa;
-				messageBytes[1] = 0x00;
-				messageBytes[2] = (byte)messageCount;
-				messageBytes[3] = (byte)messageText.Length;
-
-				Array.Copy(UTF8Encoding.UTF8.GetBytes(messageText), 0, messageBytes, 4, messageBytes[3]);
-
-				Console.WriteLine($"{DateTime.Now:HH:mm:ss}-TX to 0x{messageBytes[0]:X2} from 0x{messageBytes[1]:X2} count {messageBytes[2]} length {messageBytes[3]} \"{messageText}\"");
-#else
-				byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(messageText);
-
-				Console.WriteLine($"{DateTime.Now:HH:mm:ss}- Length {messageBytes.Length} \"{messageText}\"");
-#endif
-
-				messageCount += 1;
-
-				sX127XDevice.Send(messageBytes);
+				sX127XDevice.OnReceive += SX127XDevice_OnReceive;
+				sX127XDevice.Receive();
+				sX127XDevice.OnTransmit += SX127XDevice_OnTransmit;
 
 				Thread.Sleep(10000);
+
+				while (true)
+				{
+					string messageText = "Hello LoRa from .NET Core! " + messageCount.ToString();
+					messageCount += 1;
+
+					byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(messageText);
+					Console.WriteLine("Sending {0} bytes message {1}", messageBytes.Length, messageText);
+					sX127XDevice.Send(messageBytes);
+
+					Thread.Sleep(50000);
+				}
 			}
-		}
 
-		private static void SX127XDevice_OnReceive(object sender, SX127XDevice.OnDataReceivedEventArgs e)
-		{
-			string messageText;
-
-#if LORA_DUPLEX
-			if ((e.Data[0] != 0x00) && (e.Data[0] != 0xFF))
+			private static void SX127XDevice_OnReceive(object sender, SX127XDevice.OnDataReceivedEventArgs e)
 			{
-#if DEBUG
-				Console.WriteLine($"{DateTime.UtcNow:hh:mm:ss}-RX to 0x{e.Data[0]:X2} from 0x{e.Data[1]:X2} invalid address");
-#endif
-				return;
+				try
+				{
+					string messageText = UTF8Encoding.UTF8.GetString(e.Data);
+
+					Console.WriteLine(@"{0:HH:mm:ss}-RX PacketSnr {1:0.0} Packet RSSI {2}dBm RSSI {3}dBm = {4} byte message ""{5}""", DateTime.Now, e.PacketSnr, e.PacketRssi, e.Rssi, e.Data.Length, messageText);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
 			}
 
-			if (e.Data.Length < 4)
+			private static void SX127XDevice_OnTransmit(object sender, SX127XDevice.OnDataTransmitedEventArgs e)
 			{
-				Console.WriteLine($"{DateTime.UtcNow:hh:mm:ss}-RX Invalid payload to short");
-
-				return;
+				Console.WriteLine("{0:HH:mm:ss}-TX Done", DateTime.Now);
 			}
-
-			// check payload not to long/short
-			if  ((e.Data[3] + 4) != e.Data.Length)
-			{
-				Console.WriteLine($"{DateTime.UtcNow:hh:mm:ss}-RX Invalid payload length");
-
-				return;
-			}
-
-			try
-			{
-				messageText = UTF8Encoding.UTF8.GetString(e.Data, 4, e.Data[3]);
-
-				Console.WriteLine($"{DateTime.Now:HH:mm:ss}-RX to 0x{e.Data[0]:X2} from 0x{e.Data[1]:X2} count {e.Data[2]} length {e.Data[3]} \"{messageText}\" snr {e.PacketSnr:0.0} packet rssi {e.PacketRssi}dBm rssi {e.Rssi}dBm ");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
-#else
-			try
-			{
-				messageText = UTF8Encoding.UTF8.GetString(e.Data);
-
-				Console.WriteLine($"{DateTime.Now:HH:mm:ss}-RX length {e.Data.Length} \"{messageText}\" snr {e.PacketSnr:0.0} packet rssi {e.PacketRssi}dBm rssi {e.Rssi}dBm ");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
-#endif
-		}
-
-		private static void SX127XDevice_OnTransmit(object sender, SX127XDevice.OnDataTransmitedEventArgs e)
-		{
-#if LORA_RECEIVER
-			sX127XDevice.SetMode(SX127XDevice.RegOpModeMode.StandBy);
-#else
-			sX127XDevice.SetMode(SX127XDevice.RegOpModeMode.ReceiveContinuous);
-#endif
-			Console.WriteLine($"{DateTime.Now:HH:mm:ss}-TX Done");
 		}
 	}
 }
-
-
-
-
